8696945a0fd769eedda22d105db94bbd245d77ae
SaadVakil <saad.vakil@afiniti.com>
2022-09-19 16:27:02 +0500

    removed homepage and added Redirection in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Activity from "./pages/Activity/Activity";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/activity/:activity_slug" element={<Activity />} />
+      <Route
+        path="/"
+        element={<Navigate to="/activity/castle-of-gerald-the-devil" replace />}
+      />
     </Routes>
   );
 }
